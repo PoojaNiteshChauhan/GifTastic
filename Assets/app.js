49d681aba25cb67b1b23c9ac0fe4e83f646a9462
Pooja Chauhan <PoojaNiteshChauhan.gmail>
2019-06-18 00:03:59 -0400

    updates

--- Assets/app.js
+++ Assets/app.js
@@ -1,21 +1,16 @@
-var topic = ["Mickey Mouse", "Popeye", " Donald Duck", "Snoopy", "Minnie Mouse", "Tom Cat", "Boo-Boo", "Jerry Mouse", "Tweety"];
+var topic = ["Mickey Mouse", "Popeye", " Donald Duck", "Snoopy", "Minnie Mouse", "Tom Cat", "Boo-Boo", "Jerry", "Tweety"];
 
 
 
 function displayButton() {
 
   var cartoonButton = 0;
-
+  $("#Buttons").empty();
   for (i = 0; i < topic.length; i++) {
-
     cartoonButton = $("<button>");
-
     cartoonButton.addClass("cartoon-btn");
-
     cartoonButton.text(topic[i]);
-
     $("#Buttons").append(cartoonButton);
-
     cartoonButton.attr("data-name", topic[i]);
   }
 }
@@ -26,65 +21,77 @@ $(document).ready(function () {
 });
 
 
-$(document).on("click", ".cartoon-btn",displaygif);
+$(document).on("click", ".cartoon-btn", displaygif);
 
-function displaygif(){
+function displaygif() {
 
   var cartoonName;
-  cartoonName = $(this).attr("data-name")        
+  cartoonName = $(this).attr("data-name")
   var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=0iQvSalSK4vJ19h4f8P5J2OYDPb9tRF1&q=" + cartoonName + "&limit=10&offset=0"
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+
+
+    var cartoonDiv = $("<div class='cartoon'>");
+
+    for (i = 0; i < 10; i++) {
+      var ratingCartoon = response.data[0].rating;
+      var pOne = $("<p>").text("Rating: " + ratingCartoon);
+      cartoonDiv.append(pOne);
+      var imgURL = response.data[i].images.fixed_width_small_still.url;
+
+      //console.log(imgURL);
+      console.log(response);
+
+      var image = $("<img>");
+      image.attr("src", imgURL);
+      image.attr({'data-animate':response.data[i].images.fixed_width_small.url});
+      image.attr({'data-still':response.data[i].images.fixed_width_small_still.url});
+      image.attr({'data-state':"still"});
+      //console.log(image);
+      cartoonDiv.append(image);
+      $("#Image").prepend(cartoonDiv);
+    }
+  })
+}
+
+
+$("#add-cartoon").on("click", function (event) {
+  event.preventDefault();
+  var cartoonAdded = $("#cartoon-input").val().trim();
+  topic.push(cartoonAdded);
+  displayButton();
+});
+
+
+
+$(document).on("click", ".Gif .cartoon img", function () {
+
   
+  var source1 = $(this).attr("src");
+  var currrentState = $(this).attr('data-state');
+  
+  if (currrentState === "still")
+  {
+    $(this).attr({'data-state':"move"});
+    source1 = $(this).attr('data-animate');
+    $(this).attr("src",source1);
+  }
+  else
+  {
+    $(this).attr({'data-state':"still"});
+    source1 = $(this).attr('data-still');
+    $(this).attr("src",source1);
+  }
+  //source1 = source1 + 
+  //if 
+  //alert(source1);
 
 
-        // Creating an AJAX call for the specific movie button being clicked
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response){
-          //console.log(response)
+  //"https://media0.giphy.com/media/fxk77tLDWIWBPmaN8Z/100w_s.gif?cid=3e4144665d0823f764566a6f63987861&rid=100w_s.gif"
+  //"https://media0.giphy.com/media/fxk77tLDWIWBPmaN8Z/100w.gif?cid=3e4144665d0823f764566a6f63987861&rid=100w.gif"
 
-          var movieDiv = $("<div class='movie'>");
-
-          // Storing the rating data
-          var rating = response.Rated;
-
-          // Creating an element to have the rating displayed
-          var pOne = $("<p>").text("Rating: " + rating);
-
-          // Displaying the rating
-          movieDiv.append(pOne);
-
-          // Storing the release year
-          var released = response.Released;
-
-          // Creating an element to hold the release year
-          var pTwo = $("<p>").text("Released: " + released);
-
-          // Displaying the release year
-          movieDiv.append(pTwo);
-
-          // Storing the plot
-          var plot = response.Plot;
-
-          // Creating an element to hold the plot
-          var pThree = $("<p>").text("Plot: " + plot);
-
-          // Appending the plot
-          movieDiv.append(pThree);
-
-          // Retrieving the URL for the image
-          var imgURL = response.Poster;
-
-          // Creating an element to hold the image
-          var image = $("<img>").attr("src", imgURL);
-
-          // Appending the image
-          movieDiv.append(image);
-
-          // Putting the entire movie above the previous movies
-          $("#buttons-view").prepend(movieDiv);
-        })
-
-      }
-
-    
+})
